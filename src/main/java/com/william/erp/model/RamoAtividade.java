90e5811d60af4082562a6761218b7f42ccf107ac
William Distler Neves <will.distlerneves@gmail.com>
2024-09-09 22:40:24 -0300

    Second commit

---
+++ src/main/java/com/william/erp/model/RamoAtividade.java
@@ -0,0 +1,62 @@
+package com.william.erp.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ramo_atividade")
+public class RamoAtividade implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "RamoAtividade [id=" + id + "]";
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RamoAtividade other = (RamoAtividade) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
---
+++ target/classes/META-INF/sql/dados-iniciais.sql
@@ -0,0 +1,15 @@
+insert into ramo_atividade (id, descricao) values (1, 'Distribuição de alimentos');
+insert into ramo_atividade (id, descricao) values (2, 'Telecomunicações');
+insert into ramo_atividade (id, descricao) values (3, 'Vestuário');
+insert into ramo_atividade (id, descricao) values (4, 'Lavanderia');
+insert into ramo_atividade (id, descricao) values (5, 'Gráfica');
+insert into ramo_atividade (id, descricao) values (6, 'Mecânica');
+insert into ramo_atividade (id, descricao) values (7, 'Turismo');
+insert into ramo_atividade (id, descricao) values (8, 'Saúde');
+insert into ramo_atividade (id, descricao) values (9, 'Educação');
+insert into ramo_atividade (id, descricao) values (10, 'Lazer');
+
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (1, '70.311.193/0001-87', 'Mercado Bom Preço', 'Mercado Bom Preço LTDA', 'ME', '2010-05-20', 1);
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (2, '52.822.994/0001-25', 'Fale Mais Telecom', 'Fale Mais Telecomunicações S/A', 'LTDA', '2005-11-15', 2);
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (3, '41.952.519/0001-57', 'Maria de Lourdes Moda', 'Maria de Lourdes Roupas LTDA', 'ME', '2012-03-05', 3);
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (4, '16.134.777/0001-89', 'Gomes Industria', 'Gomes Industria de Metais LTDA', 'EPP', '2008-07-12', 4);
